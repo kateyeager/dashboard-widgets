cce3ed78a009e4229658ff870d7ef2cbe222a605
kyeager <kyeager@ewu.edu>
2022-05-05 21:41:17 -0700

    styling and added api data point

--- js/weather.js
+++ js/weather.js
@@ -9,6 +9,8 @@ const units = 'imperial';
 // URL query string
 const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKEY}&units=${units}`;
 
+
+
 // Using fetch to get data
 fetch(url)
 .then( response => response.json() )
@@ -23,9 +25,20 @@ fetch(url)
   // Template to output
   const template = `
     <h1>Weather</h1>
+
+   
+
     <data value="${data.name}" class="city">${data.name}</data>
+ 
     <data value="${data.main.temp}" class="temp">${data.main.temp}&#8457;</data>
+
+    <br>
+    <html>Feels like </html>
+    <data value="${data.main.feels_like}" class="feels_like">${data.main.feels_like}&#8457;</data>
+
+
     <img src="http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" alt="Placeholder">
+   
   `;
 
 
