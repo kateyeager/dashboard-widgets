00f9d56da2dfda2b4781790210b1e8febc1f3617
kyeager <kyeager@ewu.edu>
2022-05-15 03:17:15 -0700

    weather and time

--- js/weather.js
+++ js/weather.js
@@ -22,24 +22,25 @@ fetch(url)
   // Get Container for Weather   
   const weatherContainer = document.querySelector('.weather');
   
-  // Template to output
+  // Template to output for Weather
   const template = `
-    <h1>Weather</h1>
 
-   
+<data value="${data.name}" class="city">${data.name}</data>
 
-    <data value="${data.name}" class="city">${data.name}</data>
- 
-    <data value="${data.main.temp}" class="temp">${data.main.temp}&#8457;</data>
+<data value="${data.main.temp}" class="temp">${data.main.temp}&#8457;</data>
 
-    <br>
-    <html>Feels like </html>
-    <data value="${data.main.feels_like}" class="feels_like">${data.main.feels_like}&#8457;</data>
+<html><span>Feels like </span></html>
+<data value="${data.main.feels_like}" class="feels_like">${data.main.feels_like}&#8457;</data>
 
+<img src="http://openweathermap.org/img/wn/${data.weather[0].icon}@4x.png" alt="Placeholder">
 
-    <img src="http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" alt="Placeholder">
-   
-  `;
+`;
+
+// <html>H: </html>
+// <data value="${data.main.temp_max}" class="temp_max">${data.main.temp_max}&#8457;</data>
+// <html>L: </html>
+// <data value="${data.main.temp_min}" class="temp_min">${data.main.temp_min}&#8457;</data>
+  
 
 
   
